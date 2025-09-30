9e5a73cf8d2879c38b81a785325cb8b56a8babf7
Rajvikaur5703 <kaurrajvi34@gmail.com>
2025-09-30 21:11:21 +0530

    Update create admin.php

--- includes/create admin.php
+++ includes/create admin.php
@@ -3,14 +3,16 @@ include '../config/config.php';
 
 // Admin details
 $username = "Rajvi Kaur";
+$dob="2004-03-10";
+$gender="Female";
 $password = password_hash("rajvi@1034", PASSWORD_BCRYPT); // secure hashing
 $email = "kaurrajvi34@gamil.com";
 $role = "admin";
 
 // Insert query
-$sql = "INSERT INTO users (name, password, email, role) VALUES (?, ?, ?, ?)";
+$sql = "INSERT INTO users (name, DOB,Gender,password, email, role) VALUES (?, ?,?,?, ?, ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("ssss", $username, $password, $email, $role);
+$stmt->bind_param("ssssss", $username, $dob,$gender,$password, $email, $role);
 
 if ($stmt->execute()) {
     echo "Admin added successfully!";
@@ -18,3 +20,4 @@ if ($stmt->execute()) {
     echo "Error: " . $conn->error;
 }
 ?>
+
