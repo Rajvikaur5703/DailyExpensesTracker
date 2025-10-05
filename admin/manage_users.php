712207785f313f6a86915a868b9b4962c9424864
syedsadiya1711-sketch <syedsadiya1711@gmail.com>
2025-10-05 16:54:36 +0530

    Update manage_users.php

--- admin/manage_users.php
+++ admin/manage_users.php
@@ -2,7 +2,7 @@
 session_start();
 include '../config/config.php';
 
-$sql="select * from users";
+$sql="select * from users where role='user'";
 $result=mysqli_query($conn,$sql);
 
 ?>
@@ -25,8 +25,7 @@ $result=mysqli_query($conn,$sql);
     </header>
 
     <div class="container">
-        <h2>Manage Users</h2>
-        <a href="#" class="add-user">+ Add New User</a>
+        <h2>Total Users</h2>
 
         <table>
             <thead>
@@ -35,6 +34,7 @@ $result=mysqli_query($conn,$sql);
                     <th>Email</th>
                     <th>Gender</th>
                     <th>DOB</th>
+                    <th>Report</th>
                 </tr>
             </thead>
             <tbody>
@@ -48,6 +48,9 @@ $result=mysqli_query($conn,$sql);
                         echo"<td>".$row['email']."</td>";
                         echo"<td>".$row['Gender']."</td>";
                         echo"<td>".$row['DOB']."</td>";
+                        echo "<td><form method='POST' action='generate_report.php'>
+                                    <button type='submit' class='btnreport' name='generate_report'>View Report</button>
+                                  </form></td>";
                         echo"</tr>";
                     }
                 }
@@ -61,70 +64,3 @@ $result=mysqli_query($conn,$sql);
     </div>
 </body>
 </html>
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manage Users - Admin</title>
-    <link rel="stylesheet" href="../style/manage_users.css">
-</head>
-<body>
-    <header>
-        <h1>Admin Panel</h1>
-        <nav>
-            <a href="admin_dashboard.php" style="color:white; margin-right:15px;">Dashboard</a>
-            <a href="manage_users.php" style="color:white;">Manage Users</a>
-        </nav>
-    </header>
-
-    <div class="container">
-        <h2>Manage Users</h2>
-        <a href="#" class="add-user">+ Add New User</a>
-
-        <table>
-            <thead>
-                <tr>
-                    <th>User Id</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Rajvi Kaur</td>
-                    <td>rajvi@gmail.com</td>
-                    <td>User</td>
-                    <td class="actions">
-                        <button class="edit-btn">Edit</button>
-                        <button class="delete-btn">Delete</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Syed Sadiya</td>
-                    <td>sadiya@gmail.com</td>
-                    <td>Admin</td>
-                    <td class="actions">
-                        <button class="edit-btn">Edit</button>
-                        <button class="delete-btn">Delete</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Amit Sharma</td>
-                    <td>amit@gmail.com</td>
-                    <td>User</td>
-                    <td class="actions">
-                        <button class="edit-btn">Edit</button>
-                        <button class="delete-btn">Delete</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-</body>
-</html> -->
